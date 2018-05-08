df6d6b850a9b93e1cb70e9290c0284da94f8a5d8
janniezhong <janniezhong@jannies-mbp>
2018-05-07 23:16:18 -0700

    Got switching screens to work

--- 2018CSFinal/src/DrawingSurface.java
+++ 2018CSFinal/src/DrawingSurface.java
@@ -1,23 +1,69 @@
+import java.awt.Point;
+import java.awt.Rectangle;
+
 import processing.core.PApplet;
 import processing.core.PImage;
 
 public class DrawingSurface extends PApplet {
 
-	private PImage background;
+	//private PImage background;
+	private PImage editable;
+	public static int screenNum;
 	public DrawingSurface() {
+		screenNum = 1;
 		runSketch();
 	}
 	
 	public void setup() {
-		background = loadImage("IntroPic.jpg");
+		//background = loadImage("IntroPic.jpg");
 	}
 	
 	public void draw() {
 		
-		background(background);   
-		fill(255);
-		textAlign(CENTER);
-		//image(background, 0, 0);
+		
+		if (mousePressed) {
+			mousePressed();
+		}
+		if (screenNum == 1) {
+		introPanel();
+		} else if (screenNum == 2) {
+			instructionsPanel();
+		}
+	
 	}
+	
+	public void introPanel() {
+		editable = loadImage("IntroPic.jpg");
+		editable.resize(width,height); 
+		//background(editable);   
+		noFill();
+		textAlign(CENTER);
+		image(editable, 0, 0);
+
+
+	}
+	
+	public void instructionsPanel() {
+		background(255);
+	}
+	
+	
+	public void mousePressed() {
+		if (screenNum == 1) {
+			//if (mouseX)
+			
+			Rectangle a = new Rectangle((int) (0.107142857*width), (int) (0.423*height),  (int)(0.2642857*width), (int) (0.05*height));
+			Rectangle b = new Rectangle((int)(0.7*width), (int) (0.423*height), (int) (0.107142857*width), (int) (0.05*height));
+
+			
+			Point p = new Point(mouseX, mouseY);
+			
+			if (a.contains(p) || b.contains(p)) {
+				screenNum = 2;
+			}
+			
+		}
+	}
+	
 	
 }
