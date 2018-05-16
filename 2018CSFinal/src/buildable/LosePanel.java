4af85dd13681e667fe7abe38e51941c4a3fa6e82
janniezhong <janniezhong@jannies-mbp>
2018-05-16 10:56:16 -0700

    updating code to match uml

---
+++ 2018CSFinal/src/buildable/LosePanel.java
@@ -0,0 +1,13 @@
+package buildable;
+
+import processing.core.PApplet;
+
+public class LosePanel {
+
+	public void draw(PApplet drawer) {
+		drawer.background(255);
+		drawer.stroke(0);
+		drawer.text("YOU LOSE", (float)(drawer.width *0.5), (float)(drawer.height *0.5));
+		drawer.image(drawer.loadImage("shelbyface.png") /*.resize(w, h)*/, (float)(drawer.width *0.25), (float)(drawer.height *0.25));
+	}
+}
