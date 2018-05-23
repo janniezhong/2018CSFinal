5ef5f69415fefd2296596a56457773975fdd427f
janniezhong <janniezhong@jannies-mbp>
2018-05-22 23:06:26 -0700

    lose panel functional

--- 2018CSFinal/src/LosePanel.java
+++ 2018CSFinal/src/LosePanel.java
@@ -1,6 +1,7 @@
 
 
 import processing.core.PApplet;
+import processing.core.PImage;
 /**
  * 
  * @author jzhong672
@@ -11,7 +12,8 @@ public class LosePanel {
 	public void draw(PApplet drawer) {
 		drawer.background(255);
 		drawer.stroke(0);
-		drawer.text("YOU LOSE", (float)(drawer.width *0.5), (float)(drawer.height *0.5));
-		drawer.image(drawer.loadImage("shelbyface.png") /*.resize(w, h)*/, (float)(drawer.width *0.25), (float)(drawer.height *0.25));
+		PImage editable = drawer.loadImage("LosePanel.jpg");
+		editable.resize(drawer.width, drawer.height);
+		drawer.image(editable, 0, 0);
 	}
 }
